a45ed99dadde55f6cc54f3ca0a0f515501f7015d
sam <surickj@gmail.com>
2021-09-09 22:48:58 -0700

    commit tree 0d658bcd301f8986f19f83035b3bd441a276b2ab

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,8 @@
 class ApplicationController < ActionController::API
+rescue_from ActiveRecord::RecordInvalid, with: 
+:render_unprocessable_entity_responce
+
   include ActionController::Cookies
+  
 
 end
